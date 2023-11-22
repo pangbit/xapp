8577e3fd825b48a481b11f2feb83ce2e11d81397
xbc <xubochen365@foxmail.com>
2023-11-22 19:16:19 +0800

    feat, base64/url encode+decode, clean, copy

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,46 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use base64::{engine::general_purpose, Engine as _};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn base64_decode(input: &str) -> String {
+    match general_purpose::STANDARD.decode(input) {
+        Ok(bs) => String::from_utf8_lossy(&bs).to_string(),
+        Err(e) => format!("Decode Err: '{e:?}'"),
+    }
+}
+
+#[tauri::command]
+fn base64_encode(input: &str) -> String {
+    general_purpose::STANDARD.encode(input)
+}
+
+#[tauri::command]
+fn url_encode(input: &str) -> String {
+    urlencoding::encode(input).to_string()
+}
+
+#[tauri::command]
+fn url_decode(input: &str) -> String {
+    match urlencoding::decode(input) {
+        Ok(s) => String::from_utf8_lossy(s.as_bytes()).to_string(),
+        Err(e) => format!("Decode Err: '{e:?}'"),
+    }
 }
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_window::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .invoke_handler(tauri::generate_handler![
+            base64_encode,
+            base64_decode,
+            url_encode,
+            url_decode
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,66 @@
 const { invoke } = window.__TAURI__.primitives;
+const { writeText } = window.__TAURI__.clipboardManager;
 
-let greetInputEl;
-let greetMsgEl;
+let xTextEl;
+let base64EncodeBtn;
+let base64DecodeBtn;
+let urlEncodeBtn;
+let urlDecodeBtn;
+let cleanBtn;
+let copyBtn;
 
-async function greet() {
+async function base64decode() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  xTextEl.value = await invoke("base64_decode", { input: xTextEl.value });
+}
+
+async function base64encode() {
+  xTextEl.value = await invoke("base64_encode", { input: xTextEl.value });
+}
+
+async function urlencode() {
+  xTextEl.value = await invoke("url_encode", { input: xTextEl.value });
+}
+
+async function urldecode() {
+  xTextEl.value = await invoke("url_decode", { input: xTextEl.value });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  xTextEl = document.querySelector("#xtext");
+
+  base64DecodeBtn = document.querySelector("#btn-base64-decode");
+  base64EncodeBtn = document.querySelector("#btn-base64-encode");
+
+  urlDecodeBtn = document.querySelector("#btn-url-decode");
+  urlEncodeBtn = document.querySelector("#btn-url-encode");
+
+  cleanBtn = document.querySelector("#btn-clean");
+  copyBtn = document.querySelector("#btn-copy");
+
+  base64DecodeBtn.addEventListener("click", () => {
+    base64decode();
   });
+
+  base64EncodeBtn.addEventListener("click", () => {
+    base64encode();
+  });
+
+  urlDecodeBtn.addEventListener("click", () => {
+    urldecode();
+  });
+
+  urlEncodeBtn.addEventListener("click", () => {
+    urlencode();
+  });
+
+  cleanBtn.addEventListener("click", () => {
+    xTextEl.value = "";
+  })
+
+  copyBtn.addEventListener("click", () => {
+    writeText(xTextEl.value);
+  });
+
+  console.log("hello tauri");
 });
